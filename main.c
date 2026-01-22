08f4e691a3ad92ede2f4838f5417ad768151d564
Umar <umarsajidm989@gmail.com>
2026-01-22 14:47:38 +0100

    added the structs for data and philo

--- main.c
+++ main.c
@@ -4,6 +4,28 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef struct s_data{
+
+    int no_of_philo;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int no_of_times_each_philo_must_eat;
+    int someone_die;
+}   t_data;
+
+typedef struct s_philo{
+    pthread_t       tid;
+    int             id;
+    int             meals_eaten;
+    long long       last_meal_time;
+    pthread_mutex_t *right_mutex;
+    pthread_mutex_t *left_mutex;
+    struct s_data   *data;
+}      t_philo;
+
+
+
 long long gettimeoftheday(void)
 {
     struct timeval tv;
@@ -28,6 +50,27 @@ void ft_usleep(long long time_in_ms)
     printf("executed");
 }
 
+void init_data(t_philo *philos, t_data *data)
+{
+    // Write the loop here
+    // Assign ID
+    // Assign Left Fork pointer
+    // Assign Right Fork pointer (Use the % formula!)
+    // Assign data pointer
+}
+
+
+void init_philos(t_philo *philo, t_data *data)
+{
+    int i = 0;
+    while (data->no_of_philo > 0)
+    {
+        philo->id == i++;
+        philo->left_mutex == fork[i];
+        philo->right_mutex == fork[(i + 1) % data->no_of_philo];
+        philo->s_data = data;
+    }
+}
 
 
 int main(int ac, char **av)
